c4a95a8329f075c65fccff3ba340b0835c73cdc0
LEE <154117403+1eegitHub@users.noreply.github.com>
2024-07-12 15:59:14 +0900

    commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         has_many :prototypes
+         as_many :prototypesh
 
 end
